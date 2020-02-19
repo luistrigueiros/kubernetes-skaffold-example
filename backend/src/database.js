fb2fbe7ccd02c794260f3eee0eeb0b1e9884c551
Luis Oscar Trigueiros <ltrigueiros@deloitte.ie>
2020-02-19 18:57:07 +0000

    Now using dotenv

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,21 +1,30 @@
-const {Pool} = require('pg')
-const pool = new Pool({
-  user: 'admin',
-  host: 'postgres-db',
-  database: 'mydb',
-  password: 'secret',
-  port: 5431,
-})
+const dotenv = require('dotenv');
+const {Pool} = require('pg');
 
+let pool = null;
+
+function initPool() {
+  if (pool != null) {
+    return;
+  }
+  dotenv.config();
+
+  pool = new Pool({connectionString: process.env.DATABASE_URL})
+
+  pool.on('connect', () => {
+    console.log('connected to the db');
+  });
+
+  pool.on('error', (err, client) => {
 // the pool will emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
-});
-
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+  });
+}
 
 function doQuery(callback) {
+  initPool();
   pool.query('SELECT NOW()', (err, res) => {
     if (err) {
       throw err;
