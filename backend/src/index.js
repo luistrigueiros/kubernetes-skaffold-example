836497e41c0490fe6b4450ce76a33fc990b60556
Luis Oscar Trigueiros <ltrigueiros@deloitte.ie>
2020-03-04 12:26:27 +0000

    Added backend 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const db = require('./database');
 const app = express();
 const port = 3000;
+const serviceName = process.env.SERVICE_NAME || 'default-service-name'
+
 
 app.get('/', (req, res) => {
   db.doQuery((row) => {
-    res.send('The date is now ' + row.now);
+    res.send(`The date is now ${row.now} at ${serviceName}`);
   });
 });
 
